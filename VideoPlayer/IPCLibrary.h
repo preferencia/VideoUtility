fa54165bde0c41340f7187077e7febaf25a66c0b
preferencia <preferencia@naver.com>
2016-04-10 15:01:19 +0900

    Useless FIle

--- VideoPlayer/IPCLibrary.h
+++
@@ -1,7 +0,0 @@
-// IPCLibrary.h
-
-#pragma once
-
-static void						SendVideoDataToRenderer(void* pvData, UINT uiDataSize);
-static void						SendPlayEndSignal();
-static unsigned __stdcall		VideoStreamPipeCheckThread(void* pParam);
